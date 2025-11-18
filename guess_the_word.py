da998053c27ad19818e5147ed20cefa695cafc2e
Mariah Maloof <mar2255237@maricopa.edu>
2025-11-17 21:14:21 -0700

    Add play_game() function and __main__ guard to fix test import issues

--- guess_the_word.py
+++ guess_the_word.py
@@ -3,60 +3,58 @@ import random
 # List of words (fruits)
 word_list = ["apple", "banana", "cherry", "orange", "grape"]
 
-# Maximum number of incorrect attempts per word
-MAX_ATTEMPTS = 10
+MAX_ATTEMPTS = 6
 
-print("Welcome to the Word Guessing Game!")
-print("Hint: The word is a type of fruit!\n")
+# Function to display word progress (used for testing)
+def display_word_progress(word, guessed_letters):
+    return " ".join([letter if letter in guessed_letters else "_" for letter in word])
 
-# Main loop for replaying the game
-while True:
-    # Pick a random word
-    word = random.choice(word_list)
-    guessed_letters = set()
-    attempts = MAX_ATTEMPTS
+# Function to play the game
+def play_game():
+    print("Welcome to the Word Guessing Game!")
+    print("Hint: The word is a type of fruit!\n")
 
-    print("New word! Try to guess it.")
-
-    # Word guessing loop
-    while attempts > 0:
-        # Show current progress
-        display = "".join([letter if letter in guessed_letters else "_" for letter in word])
-        print("Word: ", " ".join(display))
-
-        # Check if the word has been guessed
-        if set(word) == guessed_letters:
-            print(f"Congratulations! You guessed the word '{word}'!")
-            break
-
-        # Ask for user input
-        guess = input("Guess a letter: ").lower()
-
-        # Validate input
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single letter.")
-            continue
-
-        # Check guess
-        if guess in word:
-            print(f"Good job! '{guess}' is in the word.")
-            guessed_letters.add(guess)
-        else:
-            attempts -= 1
-            print(f"Wrong guess. You have {attempts} attempts left.")
-
-    # If attempts run out
-    if attempts == 0:
-        print(f"Game Over! Better luck next time... The word was '{word}'. Try again!")
-
-    # Ask if the user wants to play again
     while True:
-        play_again = input("Do you want to play again? (y/n): ").lower()
-        if play_again == "y":
-            print("\nStarting a new word...\n")
-            break  # Restart outer loop
-        elif play_again == "n":
-            print("Thanks for playing! Goodbye!")
-            exit()
-        else:
-            print("Please enter 'y' for yes or 'n' for no.")
+        word = random.choice(word_list)
+        guessed_letters = set()
+        attempts = MAX_ATTEMPTS
+        print("New word! Try to guess it.")
+
+        while attempts > 0:
+            display = display_word_progress(word, guessed_letters)
+            print("Word: ", display)
+
+            if set(word) == guessed_letters:
+                print(f"Congratulations! You guessed the word '{word}'!\n")
+                break
+
+            guess = input("Guess a letter: ").lower()
+
+            if len(guess) != 1 or not guess.isalpha():
+                print("Please enter a single letter.")
+                continue
+
+            if guess in word:
+                print(f"Good job! '{guess}' is in the word.")
+                guessed_letters.add(guess)
+            else:
+                attempts -= 1
+                print(f"Wrong guess. You have {attempts} attempts left.")
+
+        if attempts == 0:
+            print(f"Game Over! The word was '{word}'.")
+
+        while True:
+            play_again = input("Do you want to play again? (y/n): ").lower()
+            if play_again == "y":
+                print("\nStarting a new word...\n")
+                break
+            elif play_again == "n":
+                print("Thanks for playing! Goodbye!")
+                return
+            else:
+                print("Please enter 'y' for yes or 'n' for no.")
+
+# Only run the game if this file is executed directly
+if __name__ == "__main__":
+    play_game()
